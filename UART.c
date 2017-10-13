96d1b8cd71291b9a36b258701f5dcb7b7a5aee5a
amepila <ie704453@iteso.mx>
2017-10-12 19:46:58 -0500

    Driver Uart

--- UART.c
+++ UART.c
@@ -12,54 +12,51 @@ void UART0_Status_IRQHandler(void){
 }
 
 void UART_init(UART_ChannelType uartChannel, uint32 systemClk, UART_BaudRateType baudRate){
-	float uartBaudrate;
-	float bRFD;
+	float bRFD = 0;
+	float sBR;
+	uint32 sbrHigh;
+	uint32 sbrLow;
+	uint32 sbrTemp;
+	//float diff;
 	uint8 bRFA;
+
+	sBR = (float)(systemClk/(baudrate*16));
+	//diff = sBr - (uint32)sBR;
+	//BRFD = 30/32 = 0.9375
+	//BRFA = 11110
+	bRFA = 0x1E;
+	sbrTemp = (uint32)sBR;
+	sbrHigh = sbrTemp & 0x1F00;
+	sbrLow = sbrTemp & 0xFF;
 
 	switch(uartChannel)
 	{
 	case UART_0:
-		uartBaudrate = ((float)(systemClk))/((16*((float)baudRate))+ bRFD);
-		bRFD = 0;
-		//bRFD = (float)bRFA/32;
-		SIM->SCGC4 |= 0x0400;
-
-		break;
-	case UART_1:
-		SIM->SCGC4 |= 0x0800;
-
-		break;
-	case UART_2:
-		SIM->SCGC4 |= 0x1000;
-
-		break;
-	case UART_3:
-		SIM->SCGC4 |= 0x2000;
-
-		break;
-	case UART_4:
-		SIM->SCGC1 |= 0x0400;
-
-		break;
-	case UART_5:
-		SIM->SCGC1 |= 0x0800;
-
-		break;
-	default:
-		break;
-	}
+	SIM->SCGC4 |= 0x0400;
+	UART0->C2 &= ~(0x04);
+	UART0->C2 &= ~(0x08);
+	UART0->BDH |= sbrHigh<<BIT8;
+	UART0->BDL |= sbrLow;
+	UART0->C4 |= bRFA;
+	UART0->C2 |= (0x04);
+	UART0->C2 |= (0x08);
 
 }
 
 void UART0_interruptEnable(UART_ChannelType uartChannel){
-
+	while (!(UART0->S1 & UART_S1_RDRF_MASK)){
+		UART0->C2 |= 0x02;
+	}
 }
 
 void UART_putChar (UART_ChannelType uartChannel, uint8 character){
-
+	while(!(UART0_S1 & UART_S1_TC_MASK)){
+		UART0->D |= character;
+	}
 }
 
 void UART_putString(UART_ChannelType uartChannel, sint8* string){
+
 
 }
 
